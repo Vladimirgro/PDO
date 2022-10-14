f66aa5b3e8efee61ff5eb480dac9f7f8fc3a73bf
Vladimir <vlamag_5@hotmail.com>
2022-10-14 13:46:43 -0500

    version 1

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PDO</title>
+	<link rel="stylesheet" type="text/css" href="librerias/bootstrap4/bootstrap.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+<body>
+
+
+
+
+	<div class="container">
+		<div class="row">
+			<h2>Crud con PDO y MySQL</h2>
+			<div class="col-sm-12">
+				<div class="card text-left">
+					<div class="card-header">
+						<ul class="nav nav-tabs card-header-tabs">
+							<li class="nav-item">
+								<a class="nav-link active" href="#">Crud PDO</a>
+							</li>
+						</ul>
+					</div>
+					<div class="card-body">
+						<div class="row">
+							<div class="col-sm-12">
+								<span class="btn btn-primary" data-toggle="modal" data-target="#insertarModal">
+									<i class="fas fa-plus-circle"></i> Nuevo registro
+								</span>
+							</div>
+						</div>
+						<hr>
+						<div class="row">
+							<div class="col-sm-12">
+                  <div id="tablaDatos"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+	<?php require_once "modalInsert.php" ?>
+	<?php require_once "modalUpdate.php" ?>
+
+	<script src="librerias/bootstrap4/jquery-3.4.1.min.js"></script>
+	<script src="librerias/bootstrap4/popper.min.js"></script>
+	<script src="librerias/bootstrap4/bootstrap.min.js"></script>
+	<script src="librerias/sweetalert.min.js"></script>
+	<script src="js/crud.js"></script>
+
+
+	<script type="text/javascript">
+		mostrar();
+	</script>
+
+</body>
+</html>
